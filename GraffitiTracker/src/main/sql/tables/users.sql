8a080f63683572bdfa0dd154c51b022ceef68066
ccaper <christian.caperton@gmail.com>
2013-12-24 08:13:43 -0600

    implemented sha-256 password hash for authentication

--- GraffitiTracker/src/main/sql/tables/users.sql
+++ GraffitiTracker/src/main/sql/tables/users.sql
@@ -6,7 +6,7 @@ CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `is_active` bit(1) NOT NULL DEFAULT b'1',
   `register_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `password` varchar(32) NOT NULL,
+  `password` varchar(64) NOT NULL,
   `last_login` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `userId_UNIQUE` (`user_id`),
