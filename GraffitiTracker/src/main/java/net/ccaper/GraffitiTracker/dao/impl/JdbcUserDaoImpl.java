bf8039cbcac1df6149c091c8d67fe755df2f499d
ccaper <christian.caperton@gmail.com>
2013-12-30 11:34:40 -0600

    removing unnecessary import

--- GraffitiTracker/src/main/java/net/ccaper/GraffitiTracker/dao/impl/JdbcUserDaoImpl.java
+++ GraffitiTracker/src/main/java/net/ccaper/GraffitiTracker/dao/impl/JdbcUserDaoImpl.java
@@ -9,7 +9,6 @@ import java.util.Map;
 
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
-import org.springframework.stereotype.Repository;
 
 import net.ccaper.GraffitiTracker.dao.UserDao;
 import net.ccaper.GraffitiTracker.enums.RoleEnum;
