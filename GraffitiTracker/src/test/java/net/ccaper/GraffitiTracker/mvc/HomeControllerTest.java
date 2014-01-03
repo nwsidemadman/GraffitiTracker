e25dfdc7d134c6f93977d5d9dae9c09fa603fe3d
ccaper <christian.caperton@gmail.com>
2014-01-03 23:03:38 +0000

    Move test of controller to correct package

--- GraffitiTracker/src/test/java/net/ccaper/GraffitiTracker/mvc/HomeControllerTest.java
+++ GraffitiTracker/src/test/java/net/ccaper/GraffitiTracker/mvc/HomeControllerTest.java
@@ -1,6 +1,6 @@
-package net.ccaper.GraffitiTracker;
+package net.ccaper.GraffitiTracker.mvc;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,7 +11,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.springframework.ui.Model;
 
 public class HomeControllerTest {
 
