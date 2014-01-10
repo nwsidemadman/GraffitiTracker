1f919c79bd6c6eb9d6a9553bb2f22591096562ec
ccaper <christian.caperton@gmail.com>
2014-01-10 16:37:45 -0600

    changing login_addresses default values for two columns

--- GraffitiTracker/src/main/sql/tables/login_addresses.sql
+++ GraffitiTracker/src/main/sql/tables/login_addresses.sql
@@ -3,8 +3,8 @@ DROP TABLE `login_addresses`;
 CREATE TABLE `login_addresses` (
   `user_id` int(10) unsigned NOT NULL,
   `inet_address` int(4) unsigned NOT NULL,
-  `number_visits` int(10) unsigned NOT NULL DEFAULT '0',
-  `last_visit_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `number_visits` int(10) unsigned NOT NULL DEFAULT '1',
+  `last_visit_timestamp` timestamp NOT NULL,
   PRIMARY KEY (`user_id`,`inet_address`),
   CONSTRAINT `user_id_login_addesses` FOREIGN KEY (`user_id`) REFERENCES `app_users` (`user_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
