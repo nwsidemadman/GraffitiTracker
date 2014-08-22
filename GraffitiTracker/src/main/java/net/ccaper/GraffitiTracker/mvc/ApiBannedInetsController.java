ba7cc34525be0cd71c44345da14c933e896d295c
ccaper <christian.caperton@gmail.com>
2014-08-22 16:22:42 -0500

    added security around banned inets api

--- GraffitiTracker/src/main/java/net/ccaper/GraffitiTracker/mvc/ApiBannedInetsController.java
+++ GraffitiTracker/src/main/java/net/ccaper/GraffitiTracker/mvc/ApiBannedInetsController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-// TODO: add security
 @RestController
 @RequestMapping("/api/banned_inets")
 @Scope("session")
