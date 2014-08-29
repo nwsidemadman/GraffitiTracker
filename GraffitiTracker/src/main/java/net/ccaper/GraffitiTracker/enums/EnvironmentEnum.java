b53ab1793568d672a2df17823897a7a9b0751bf7
ccaper <christian.caperton@gmail.com>
2014-08-29 14:41:34 -0500

    adding javadoc for enums

--- GraffitiTracker/src/main/java/net/ccaper/GraffitiTracker/enums/EnvironmentEnum.java
+++ GraffitiTracker/src/main/java/net/ccaper/GraffitiTracker/enums/EnvironmentEnum.java
@@ -2,6 +2,11 @@ package net.ccaper.GraffitiTracker.enums;
 
 import org.apache.commons.lang3.StringUtils;
 
+/**
+ * 
+ * @author ccaper The Enum for development environments
+ * 
+ */
 public enum EnvironmentEnum {
   LOCAL("", "Local"), DEV("dev", "Development"), STAGE("staging", "Staging"), PROD(
       "prod", "");
@@ -9,19 +14,44 @@ public enum EnvironmentEnum {
   private String environmentPropertyString;
   private String displayString;
 
+  /**
+   * Instantiates a new environment enum.
+   * 
+   * @param environmentPropertyString
+   *          the environment property string
+   * @param displayString
+   *          the display string
+   */
   private EnvironmentEnum(String environmentPropertyString, String displayString) {
     this.environmentPropertyString = environmentPropertyString;
     this.displayString = displayString;
   }
 
+  /**
+   * Gets the environment property string.
+   * 
+   * @return the environment property string
+   */
   public String getEnvironmentPropertyString() {
     return environmentPropertyString;
   }
 
+  /**
+   * Gets the display string.
+   * 
+   * @return the display string
+   */
   public String getDisplayString() {
     return displayString;
   }
 
+  /**
+   * Gets the environment enum from environment property string.
+   * 
+   * @param environmentPropertyString
+   *          the environment property string
+   * @return the environment enum from environment property string
+   */
   public static EnvironmentEnum getEnvironmentEnumFromEnvironmentPropertyString(
       String environmentPropertyString) {
     if (StringUtils.isEmpty(environmentPropertyString)) {
@@ -35,8 +65,8 @@ public enum EnvironmentEnum {
     }
     throw new IllegalArgumentException(
         String
-        .format(
-            "The environment property string '%s' does not map to a valid EnvironmentEnum.",
-            environmentPropertyString));
+            .format(
+                "The environment property string '%s' does not map to a valid EnvironmentEnum.",
+                environmentPropertyString));
   }
 }
--- GraffitiTracker/src/main/java/net/ccaper/GraffitiTracker/enums/RoleEnum.java
+++ GraffitiTracker/src/main/java/net/ccaper/GraffitiTracker/enums/RoleEnum.java
@@ -1,5 +1,10 @@
 package net.ccaper.GraffitiTracker.enums;
 
+/**
+ * 
+ * @author ccaper Enum for Roles
+ * 
+ */
 public enum RoleEnum {
   SUPERADMIN("ROLE_SUPERADMIN", "Super Admin"), ADMIN("ROLE_ADMIN", "Admin"), BASIC(
       "ROLE_BASIC", "Basic"), SUBSCRIPTION("ROLE_SUBSCRIPTION", "Subscription"), LICENSED(
@@ -8,19 +13,44 @@ public enum RoleEnum {
   private String dbString;
   private String displayString;
 
+  /**
+   * Instantiates a new role enum.
+   * 
+   * @param dbString
+   *          the db string
+   * @param displayString
+   *          the display string
+   */
   private RoleEnum(String dbString, String displayString) {
     this.dbString = dbString;
     this.displayString = displayString;
   }
 
+  /**
+   * Gets the db string.
+   * 
+   * @return the db string
+   */
   public String getDbString() {
     return dbString;
   }
 
+  /**
+   * Gets the display string.
+   * 
+   * @return the display string
+   */
   public String getDisplayString() {
     return displayString;
   }
 
+  /**
+   * Gets the role enum from db string.
+   * 
+   * @param roleDbString
+   *          the role db string
+   * @return the role enum from db string
+   */
   public static RoleEnum getRoleEnumFromDbString(String roleDbString) {
     for (RoleEnum roleEnum : RoleEnum.values()) {
       if (roleEnum.getDbString().equals(roleDbString)) {
