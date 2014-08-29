29d35fb0ba002f76b556b54b94d1fce969abbd7c
ccaper <christian.caperton@gmail.com>
2014-08-29 13:34:02 -0500

    adding javadoc for all dao's

--- GraffitiTracker/src/main/java/net/ccaper/GraffitiTracker/dao/LoginAddressDao.java
+++ GraffitiTracker/src/main/java/net/ccaper/GraffitiTracker/dao/LoginAddressDao.java
@@ -6,10 +6,40 @@ import org.apache.commons.lang3.tuple.ImmutablePair;
 
 import net.ccaper.GraffitiTracker.objects.LoginInet;
 
+/**
+ * 
+ * @author ccaper Data Access Object for
+ *         {@link net.ccaper.GraffitiTracker.objects.LoginInet}
+ * 
+ */
 public interface LoginAddressDao {
+
+  /**
+   * Update login address by username.
+   * 
+   * @param username
+   *          the username
+   * @param ipAddress
+   *          the ip address
+   */
   void updateLoginAddressByUsername(String username, String ipAddress);
-  
+
+  /**
+   * Gets the {@link net.ccaper.GraffitiTracker.objects.LoginInet} by user id.
+   * 
+   * @param userId
+   *          the user id
+   * @return the {@link net.ccaper.GraffitiTracker.objects.LoginInet} by user id
+   */
   List<LoginInet> getLoginAddressByUserId(int userId);
-  
+
+  /**
+   * Gets the users sharing inet.
+   * 
+   * @param inet
+   *          the inet
+   * @return the users sharing inet that is an ImmutablePair where key is
+   *         username and value is userid
+   */
   List<ImmutablePair<String, Integer>> getUsersSharingInet(String inet);
 }
--- GraffitiTracker/src/main/java/net/ccaper/GraffitiTracker/utils/Encoder.java
+++ GraffitiTracker/src/main/java/net/ccaper/GraffitiTracker/utils/Encoder.java
@@ -5,6 +5,7 @@ import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 public class Encoder {
   private static final ShaPasswordEncoder passwordEncoder = new ShaPasswordEncoder(256);
 
+  
   public static String encodeString(String salt, String stringToEncode) {
     return passwordEncoder.encodePassword(stringToEncode, salt);
   }
