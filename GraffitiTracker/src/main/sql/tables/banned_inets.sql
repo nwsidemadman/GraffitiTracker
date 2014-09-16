fdd221dcc2d58d0300b307773de0401629faa64e
ccaper <christian.caperton@gmail.com>
2014-09-16 17:28:57 -0500

    fixing sql quote char again

--- GraffitiTracker/src/main/sql/tables/banned_inets.sql
+++ GraffitiTracker/src/main/sql/tables/banned_inets.sql
@@ -1,11 +1,11 @@
-DROP TABLE 'banned_inets';
+DROP TABLE `banned_inets`;
 
-CREATE TABLE 'banned_inets' (
-  'inet_min_incl' int(4) unsigned NOT NULL,
-  'inet_max_incl' int(4) unsigned NOT NULL,
-  'active' bit(1) NOT NULL DEFAULT b'1',
-  'number_registration_attempts' int(10) unsigned DEFAULT '0',
-  'notes' varchar(255) DEFAULT NULL,
-  'created_timestamp' timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY ('inet_min_incl','inet_max_incl')
+CREATE TABLE `banned_inets` (
+  `inet_min_incl` int(4) unsigned NOT NULL,
+  `inet_max_incl` int(4) unsigned NOT NULL,
+  `active` bit(1) NOT NULL DEFAULT b`1`,
+  `number_registration_attempts` int(10) unsigned DEFAULT `0`,
+  `notes` varchar(255) DEFAULT NULL,
+  `created_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`inet_min_incl`,`inet_max_incl`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
--- GraffitiTracker/src/main/sql/tables/reset_password.sql
+++ GraffitiTracker/src/main/sql/tables/reset_password.sql
@@ -1,10 +1,10 @@
-DROP TABLE 'reset_password';
+DROP TABLE `reset_password`;
 
-CREATE TABLE 'reset_password' (
-  'app_user_id' int(10) unsigned NOT NULL,
-  'unique_url_param' char(36) NOT NULL,
-  'reset_password_timestamp' timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY ('app_user_id', 'unique_url_param'),
-  UNIQUE KEY 'unique_url_path_UNIQUE' ('unique_url_param'),
-  CONSTRAINT 'app_user_id_reset_password' FOREIGN KEY ('app_user_id') REFERENCES 'app_users' ('id') ON DELETE CASCADE
+CREATE TABLE `reset_password` (
+  `app_user_id` int(10) unsigned NOT NULL,
+  `unique_url_param` char(36) NOT NULL,
+  `reset_password_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`app_user_id`, `unique_url_param`),
+  UNIQUE KEY `unique_url_path_UNIQUE` (`unique_url_param`),
+  CONSTRAINT `app_user_id_reset_password` FOREIGN KEY (`app_user_id`) REFERENCES `app_users` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
