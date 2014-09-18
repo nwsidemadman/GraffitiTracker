3b46bd681730388c14a38316df16ca201bbf3fe7
ccaper <christian.caperton@gmail.com>
2014-09-18 16:56:28 -0500

    adding javadoc

--- GraffitiTracker/src/main/java/net/ccaper/graffitiTracker/serviceImpl/ChicagoCityServicesGraffitiServiceImpl.java
+++ GraffitiTracker/src/main/java/net/ccaper/graffitiTracker/serviceImpl/ChicagoCityServicesGraffitiServiceImpl.java
@@ -5,8 +5,6 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -14,12 +12,17 @@ import org.springframework.stereotype.Service;
 
 import net.ccaper.graffitiTracker.dao.ChicagoCityServicesGraffitiDao;
 import net.ccaper.graffitiTracker.dao.ChicagoCityServicesServerDao;
-import net.ccaper.graffitiTracker.dao.impl.JdbcChicagoCityServicesGraffitiDaoImpl;
-import net.ccaper.graffitiTracker.dao.impl.RestChicagoCityServicesServerDaoImpl;
 import net.ccaper.graffitiTracker.objects.ChicagoCityServiceGraffiti;
 import net.ccaper.graffitiTracker.service.ChicagoCityServicesGraffitiService;
 import net.ccaper.graffitiTracker.spring.AppConfig;
 
+/**
+ * 
+ * @author ccaper
+ * 
+ *         Implementation for the ChicagoCityServicesGraffitiService
+ *
+ */
 @Service("chicagoCityServicesGraffitiService")
 public class ChicagoCityServicesGraffitiServiceImpl implements
     ChicagoCityServicesGraffitiService {
@@ -29,27 +32,52 @@ public class ChicagoCityServicesGraffitiServiceImpl implements
   private ChicagoCityServicesGraffitiDao chicagoCityServicesGraffitiDao;
 
   // visible for testing
-  // TODO(ccaper): javadoc
+  /**
+   * Sets the chicago city services server dao.
+   *
+   * @param chicagoCityServicesServerDao
+   *          the new chicago city services server dao
+   */
   void setChicagoCityServicesServerDao(
       ChicagoCityServicesServerDao chicagoCityServicesServerDao) {
     this.chicagoCityServicesServerDao = chicagoCityServicesServerDao;
   }
 
   // visible for testing
-  // TODO(ccaper): javadoc
+  /**
+   * Sets the chicago city services graffiti dao.
+   *
+   * @param chicagoCityServicesGraffitiDao
+   *          the new chicago city services graffiti dao
+   */
   void setChicagoCityServicesGraffitiDao(
       ChicagoCityServicesGraffitiDao chicagoCityServicesGraffitiDao) {
     this.chicagoCityServicesGraffitiDao = chicagoCityServicesGraffitiDao;
   }
 
+  /*
+   * (non-Javadoc)
+   * 
+   * @see net.ccaper.graffitiTracker.service.ChicagoCityServicesGraffitiService#
+   * getChicagoCityServiceGraffitiRequestsFromServer(java.util.Date,
+   * java.util.Date)
+   */
   @Override
   public List<ChicagoCityServiceGraffiti> getChicagoCityServiceGraffitiRequestsFromServer(
       Date startDate, Date endDate) {
     return chicagoCityServicesServerDao.getGraffiti(startDate, endDate);
   }
 
+  // TODO(ccaper): unit test
+  /*
+   * (non-Javadoc)
+   * 
+   * @see net.ccaper.graffitiTracker.service.ChicagoCityServicesGraffitiService#
+   * storeChicagoCityServiceGraffitiRequests(java.util.List)
+   */
   @Override
-  public void storeChicagoCityServiceGraffitiRequests(List<ChicagoCityServiceGraffiti> data) {
+  public void storeChicagoCityServiceGraffitiRequests(
+      List<ChicagoCityServiceGraffiti> data) {
     for (ChicagoCityServiceGraffiti datum : data) {
       if (datum.getMediaUrl() != null) {
         chicagoCityServicesGraffitiDao.storeChicagoCityServicesGraffiti(datum);
@@ -57,11 +85,19 @@ public class ChicagoCityServicesGraffitiServiceImpl implements
     }
   }
 
+  /**
+   * The main method.
+   *
+   * @param args
+   *          the arguments
+   */
+  // TODO(ccaper): remove after feature added to web app
   public static void main(String[] args) {
     AnnotationConfigApplicationContext context = null;
     try {
       context = new AnnotationConfigApplicationContext(AppConfig.class);
-      ChicagoCityServicesGraffitiService service = context.getBean(ChicagoCityServicesGraffitiServiceImpl.class);
+      ChicagoCityServicesGraffitiService service = context
+          .getBean(ChicagoCityServicesGraffitiServiceImpl.class);
       Calendar cal = GregorianCalendar.getInstance();
       cal.set(2014, 7, 1, 0, 0);
       Date startDate = cal.getTime();
