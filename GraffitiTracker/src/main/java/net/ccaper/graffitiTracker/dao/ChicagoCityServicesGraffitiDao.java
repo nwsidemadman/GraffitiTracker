d1314f5096c5d58d59132f9a3e5338d3c9b19cb4
ccaper <christian.caperton@gmail.com>
2014-09-18 16:58:40 -0500

    adding javadoc

--- GraffitiTracker/src/main/java/net/ccaper/graffitiTracker/dao/ChicagoCityServicesGraffitiDao.java
+++ GraffitiTracker/src/main/java/net/ccaper/graffitiTracker/dao/ChicagoCityServicesGraffitiDao.java
@@ -2,6 +2,20 @@ package net.ccaper.graffitiTracker.dao;
 
 import net.ccaper.graffitiTracker.objects.ChicagoCityServiceGraffiti;
 
+/**
+ * 
+ * @author ccaper
+ * 
+ *         DAO for ChicagoCityServicesGraffiti
+ *
+ */
 public interface ChicagoCityServicesGraffitiDao {
+
+  /**
+   * Store chicago city services graffiti request in repository.
+   *
+   * @param graffiti
+   *          the graffiti request
+   */
   void storeChicagoCityServicesGraffiti(ChicagoCityServiceGraffiti graffiti);
 }
